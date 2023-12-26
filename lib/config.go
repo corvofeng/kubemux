c6e424f8bed236adbe4e0a26f39ec75586839ae8
corvofeng <corvofeng@gmail.com>
2023-12-26 17:45:30 +0800

    Update the parser.
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- lib/config.go
+++ lib/config.go
@@ -1,9 +1,27 @@
 package lib
 
+import (
+	"encoding/json"
+
+	"gopkg.in/yaml.v2"
+)
+
 // Config 结构体用于匹配 YAML 配置文件的结构
 type Config struct {
 	Name    string   `yaml:"name"`
 	Tmux    string   `yaml:"tmux"`
 	Root    string   `yaml:"root"`
 	Windows []Window `yaml:"windows"`
+}
+
+func ParseConfig(data []byte) (Config, error) {
+	var config Config
+	err := json.Unmarshal(data, &config)
+	return config, err
+}
+
+func ParseYAMLConfig(data []byte) (Config, error) {
+	var config Config
+	err := yaml.Unmarshal(data, &config)
+	return config, err
 }
--- lib/config_test.go
+++ lib/config_test.go
@@ -1,7 +1,113 @@
 package lib
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestConfigRead(t *testing.T) {
+func TestParseConfig(t *testing.T) {
+	configData := []byte(`{
+		"Name": "my_session",
+		"Root": "~/GitRepo",
+		"Windows": [
+			{
+				"Name": "editor",
+				"Root": "~/GitRepo",
+				"Panes": [
+					{"Command": "vim"},
+					{"Command": "ls -alh"},
+					{"Command": "pwd"}
+				]
+			},
+			{
+				"Name": "shell",
+				"Root": "/tmp",
+				"Panes": [
+					{"Command": "htop"}
+				]
+			}
+		]
+	}`)
 
+	expectedConfig := Config{
+		Name: "my_session",
+		Root: "~/GitRepo",
+		Windows: []Window{
+			{
+				Name: "editor",
+				Root: "~/GitRepo",
+				Panes: []Pane{
+					{Command: "vim"},
+					{Command: "ls -alh"},
+					{Command: "pwd"},
+				},
+			},
+			{
+				Name: "shell",
+				Root: "/tmp",
+				Panes: []Pane{
+					{Command: "htop"},
+				},
+			},
+		},
+	}
+
+	parsedConfig, err := ParseConfig(configData)
+	if err != nil {
+		t.Errorf("Error parsing config: %s", err)
+	}
+
+	if fmt.Sprintf("%+v", parsedConfig) != fmt.Sprintf("%+v", expectedConfig) {
+		t.Errorf("Parsed config doesn't match expected config")
+	}
+}
+
+func TestParseYAMLConfig(t *testing.T) {
+	yamlData := []byte(`
+name: my_session
+root: ~/GitRepo
+windows:
+  - name: editor
+    root: ~/GitRepo
+    panes:
+      - command: vim
+      - command: ls -alh
+      - command: pwd
+  - name: shell
+    root: /tmp
+    panes:
+      - command: htop
+  `)
+
+	expectedConfig := Config{
+		Name: "my_session",
+		Root: "~/GitRepo",
+		Windows: []Window{
+			{
+				Name: "editor",
+				Root: "~/GitRepo",
+				Panes: []Pane{
+					{Command: "vim"},
+					{Command: "ls -alh"},
+					{Command: "pwd"},
+				},
+			},
+			{
+				Name: "shell",
+				Root: "/tmp",
+				Panes: []Pane{
+					{Command: "htop"},
+				},
+			},
+		},
+	}
+
+	parsedConfig, err := ParseYAMLConfig(yamlData)
+	if err != nil {
+		t.Errorf("Error parsing YAML config: %s", err)
+	}
+
+	if fmt.Sprintf("%+v", parsedConfig) != fmt.Sprintf("%+v", expectedConfig) {
+		t.Errorf("Parsed YAML config doesn't match expected config:\n%s\n%s\n", parsedConfig, expectedConfig)
+	}
 }
